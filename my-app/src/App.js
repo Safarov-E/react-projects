81b50f6fcfc8cc0cccc855bf3b680c788e84ee1e
Safarov <elkhan.safarov10@yandex.com>
2020-02-17 18:19:33 +0500

    Создание запроса к db.json

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      
+    }
+  }
+  componentDidMount() {
+    fetch('/users')
+    .then((response) => response.json())
+    .then((myJson) =>  console.log(myJson))
+  }
+  render(){
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var jsonFile = require('./db.json');
+
+router.get('/', function (req, res, next) {
+	res.json(jsonFile);
+});
+
+module.exports = router;
+
