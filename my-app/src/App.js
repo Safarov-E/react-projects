a444325cd2b0d2381cc2f31853825f111966d9f5
Safarov <elkhan.safarov10@yandex.com>
2020-02-17 22:46:28 +0500

    Вывод данных на страницу

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,93 @@
 import React, { Component } from 'react';
+import './App.css'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      
+      img: null
     }
   }
 
   componentDidMount() {
     fetch('/users')
     .then((response) => response.json())
-    .then((myJson) =>  console.log(myJson))
+    .then((jsonData) => jsonData )
+    .then((jsonStr) => {
+        this.setState({ img: jsonStr });
+    });
   }
-  
+
   render(){
+    if(this.state.img === null) {
+      return <p>Loading...</p>
+    }
+    const {bestsellers, coffee, goods} = this.state.img
     return (
-      <div>
-        
+      <>
+      <div class="catalogTitle">
+        <h1 className="catalogHeaderTitle">bestsellers</h1>
       </div>
+        <div className="catalogItemContent">
+        {
+          bestsellers.map((item, index) => {
+            return( 
+            <div className="cardBox" key={index}>
+              <div className="noteBox-for-open">
+                <img src={item.url} className="catalogItemImages" width="219px"/>
+                <p className="catalogItemName">{item.name}</p> 
+                <p className="catalogItemPrice">Цена: {item.price}</p>
+                <div className="ItemButton">
+                  <button className="catalogItemButton">Купить</button>
+                </div>
+              </div>
+            </div>)
+          })
+        }
+      </div>
+      <div class="catalogTitle">
+        <h1 className="catalogHeaderTitle">coffee</h1>
+      </div>
+      <div className="catalogItemContent">
+        {
+          coffee.map((item, index) => {
+            return( 
+            <div className="cardBox" key={index}>
+              <div className="noteBox-for-open">
+                <p className="catalogItemName">{item.name}</p>
+                <p className="catalogItemCountry">Страна производитель: {item.country}</p>
+                <img src={item.url} className="catalogItemImages" width="219px"/>
+                <p className="catalogItemPrice">Цена: {item.price}</p>
+                <p className="catalogItemDescription">{item.description}</p>
+                <div className="ItemButton">
+                  <button className="catalogItemButton">Купить</button>
+                </div>
+              </div>
+            </div>)
+          })
+        }
+      </div>
+      <div class="catalogTitle">
+        <h1 className="catalogHeaderTitle">goods</h1>
+      </div>
+      <div className="catalogItemContent">
+        {
+          goods.map((item, index) => {
+            return( 
+            <div className="cardBox" key={index}>
+              <div className="noteBox-for-open">
+                <p className="catalogItemName">{item.name}</p> 
+                <img src={item.url} className="catalogItemImages" width="219px"/>
+                <p className="catalogItemPrice">Цена: {item.price}</p>
+                <div className="ItemButton">
+                  <button className="catalogItemButton">Купить</button>
+                </div>
+              </div>
+            </div>)
+          })
+        }
+      </div>
+      </>
     )
   }
 }
